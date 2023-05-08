255b6dff5a320da282c85288abb939ad177c354d
Elon Aseneka <elonsantos63@gmail.com>
2023-05-08 18:23:40 +0300

    force recreate

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { FileUploadModule } from './file-upload/file-upload.module';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: () => {
-        const url = `mongodb://${process.env.DB_HOST}/billboardz_db`;
+        const url = `mongodb://${process.env.DB_HOST}:27017/billboardz_db`;
         return {
           uri: url,
         };
